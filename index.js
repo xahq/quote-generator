6397b8ff2427324b25f9da9ae611675c6d365bc7
xahq <107444317+xahq@users.noreply.github.com>
2022-07-17 20:10:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let quote = document.getElementById("quote");
 let generate = document.getElementById("generateQuote");
 
+function onMouseEnter(){
+    if (!generate.classList.contains("hover")){
+        generate.classList.add("hover");
+    }
+}
+
+function onMouseLeave(){
+    if (generate.classList.contains("hover")){
+        generate.classList.remove("hover");
+    }
+}
+
 function getQuote(){
     return fetch("https://api.quotable.io/random") 
     .then(res => {
@@ -13,5 +25,9 @@ function getQuote(){
         window.alert("Sorry, this request Throttled");
     })
 }
+
+generate.addEventListener("mouseenter", onMouseEnter)
+
+generate.addEventListener("mouseleave", onMouseLeave)
 
 generate.addEventListener("click", getQuote)
